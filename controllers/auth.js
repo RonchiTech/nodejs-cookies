72c5d3d082a4280eeff67963d09f399cd83919e1
ronchitech <ronchibusiness@gmail.com>
2021-06-25 22:12:42 +0800

    setting up cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,13 @@
 exports.getLogin = (req, res, next) => {
+  console.log(req.get('Cookie').split('=')[1]);
+  const isLoggedIn = req.get('Cookie').split('=')[1]
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
   });
 };
+
+exports.postLogin = (req,res,next)=> {
+  res.setHeader('Set-Cookie', 'isLoggedIn=true')
+  res.redirect('/')
+}
